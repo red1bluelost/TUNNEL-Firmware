8c1c8d495380b3b63d5d4982c6682e35368e2274
Micah Weston <micahsweston@gmail.com>
2022-12-03 21:39:37 -0500

    Adds a base for the RTIC app.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+use panic_rtt_target as _;
+use rtic::app;
+
+#[app(device = hal::pac, peripherals = true, dispatchers = [SPI1])]
+mod app {
+    use stm32f4xx_hal as hal;
+
+    use rtt_target::{rprintln, rtt_init_print};
+    use systick_monotonic::Systick;
+
+    #[shared]
+    struct Shared {}
+
+    #[local]
+    struct Local {}
+
+    #[monotonic(binds = SysTick, default = true)]
+    type Tonic = Systick<1000>;
+
+    #[init]
+    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        rtt_init_print!();
+        rprintln!("init");
+
+        let mono = Systick::new(ctx.core.SYST, 48_000_000);
+
+        (Shared {}, Local {}, init::Monotonics(mono))
+    }
 }

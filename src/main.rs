4a706ddddabde62cd822d5af2b9b03d2bcdf8a70
Micah Weston <micahsweston@gmail.com>
2023-01-18 18:05:58 -0500

    Implements a working usb device that shows up when connected.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,114 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 use panic_rtt_target as _;
+// use panic_halt as _;
 
 #[rtic::app(device = hal::pac, peripherals = true, dispatchers = [SPI1])]
 mod app {
-    use cortex_m_semihosting::{debug, hprintln};
+    // use cortex_m_semihosting::{debug, hprintln};
+    use hal::{
+        gpio::*,
+        otg_fs::{UsbBus, UsbBusType, USB},
+        pac,
+        prelude::*,
+        timer,
+    };
     use rtt_target::{rprintln, rtt_init_print};
     use stm32f4xx_hal as hal;
-    use systick_monotonic::Systick;
+    use usb_device::prelude::*;
 
     #[shared]
     struct Shared {}
 
     #[local]
-    struct Local {}
+    struct Local {
+        usb_dev: UsbDevice<'static, UsbBusType>,
+        led_on_board: ErasedPin<Output>,
+    }
 
-    #[monotonic(binds = SysTick, default = true)]
-    type Tonic = Systick<1000>;
+    #[monotonic(binds = TIM2, default = true)]
+    type MicrosecMono = timer::MonoTimerUs<pac::TIM2>;
 
     #[init]
     fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
         rtt_init_print!();
         rprintln!("init");
 
-        let mono = Systick::new(ctx.core.SYST, 48_000_000);
+        static mut EP_MEMORY: [u32; 1024] = [0; 1024];
 
-        hprintln!("Hello, world!");
+        let dp = ctx.device;
+
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc
+            .cfgr
+            .use_hse(8.MHz())
+            .sysclk(48.MHz())
+            .require_pll48clk()
+            .freeze();
+
+        let mono = dp.TIM2.monotonic_us(&clocks);
+
+        let gpioa = dp.GPIOA.split();
+
+        let usb = USB {
+            usb_global: dp.OTG_FS_GLOBAL,
+            usb_device: dp.OTG_FS_DEVICE,
+            usb_pwrclk: dp.OTG_FS_PWRCLK,
+            pin_dm: gpioa.pa11.into_alternate(),
+            pin_dp: gpioa.pa12.into_alternate(),
+            hclk: clocks.hclk(),
+        };
+
+        static mut USB_BUS: Option<usb_device::bus::UsbBusAllocator<UsbBusType>> = None;
+        unsafe { USB_BUS.replace(UsbBus::new(usb, &mut EP_MEMORY)) };
+
+        let usb_dev = UsbDeviceBuilder::new(
+            unsafe { USB_BUS.as_ref().unwrap() },
+            UsbVidPid(0x16c0, 0x27dd),
+        )
+        .manufacturer("TUNNEL Team")
+        .product("TUNNEL Device")
+        .serial_number("TEST")
+        .device_class(0xff)
+        .build();
 
         // exit QEMU
         // NOTE do not run this on hardware; it can corrupt OpenOCD state
-        debug::exit(debug::EXIT_SUCCESS);
+        // debug::exit(debug::EXIT_SUCCESS);
 
-        (Shared {}, Local {}, init::Monotonics(mono))
+        let led_on_board = gpioa.pa5.into_push_pull_output().into();
+        blink::spawn_after(1.secs()).unwrap();
+
+        rprintln!("init end");
+        (
+            Shared {},
+            Local {
+                usb_dev,
+                led_on_board,
+            },
+            init::Monotonics(mono),
+        )
+    }
+
+    #[idle(local = [usb_dev])]
+    fn idle(ctx: idle::Context) -> ! {
+        loop {
+            if ctx.local.usb_dev.poll(&mut []) {
+                rprintln!("usb!");
+            }
+        }
+    }
+
+    /// This task is just an indicator that the board works.
+    #[task(local = [led_on_board])]
+    fn blink(ctx: blink::Context) {
+        let led = ctx.local.led_on_board;
+        if led.is_set_low() {
+            led.set_high();
+        } else {
+            led.set_low();
+        }
+        blink::spawn_after(1.secs()).unwrap();
     }
 }
